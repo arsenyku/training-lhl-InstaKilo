f6a8520533bc5a6a21e4ab2fa324b80c190cbb8a
asu <arsenyk.ustaris.dev@gmail.com>
2015-09-09 10:54:25 -0700

    Initial Commit

---
+++ InstaKilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaKilo
+//
+//  Created by asu on 2015-09-09.
+//  Copyright (c) 2015 asu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
